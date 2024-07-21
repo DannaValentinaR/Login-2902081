8cf7c9c567feb23f9ed4af8c4f699b3c33cff055
DannaValentinaR <dvramirez078@misena.edu.co>
2024-07-20 23:32:39 -0500

    feature: se agrego HeadingTextComponent y MyTextFieldComponent a AppComponents

--- app/src/main/java/co/edu/sena/com/example/login2902081/components/AppComponents.kt
+++ app/src/main/java/co/edu/sena/com/example/login2902081/components/AppComponents.kt
@@ -2,14 +2,22 @@ package co.edu.sena.com.example.login2902081.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import co.edu.sena.com.example.login2902081.R
+import co.edu.sena.com.example.login2902081.ui.theme.PurpleGrey40
 
 @Composable
 fun NormalTextComponent(value: String) {
@@ -17,11 +25,56 @@ fun NormalTextComponent(value: String) {
         text = value,
         modifier = Modifier
             .fillMaxWidth()
-            .heightIn(min = 80.dp),
+            .heightIn(min = 40.dp),
         style = TextStyle(
-            fontSize = 18.sp,
+            fontSize = 24.sp,
             fontWeight = FontWeight.Normal,
             fontStyle = FontStyle.Normal
-        )//TextStyle
-    )// Text
-}// NormalTextComponent
+        ),
+        color = colorResource(id = R.color.black),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun HeadingTextComponent(value: String) {
+    Text(
+        text = value,
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(),
+        style = TextStyle(
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Normal
+        ),
+        color = colorResource(id = R.color.black),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun MyTextFieldComponent(labelValue: String) {
+
+    val textValue = remember {
+        mutableStateOf("")
+    }
+
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        label = { Text(text = labelValue) },
+        colors = TextFieldDefaults.outlinedTexteldColors(
+            focusedBorderColor = PurpleGrey40,
+            focusedLabelColor = PurpleGrey40,
+            cursorColor = PurpleGrey40,
+            backgrundColor = PurpleGrey40
+
+        ),
+        keyboardOptions = keyboardOptions.Default,
+        value = textValue.value,
+        onValueChange = {
+            textValue.value = it
+        },
+
+        )
+}

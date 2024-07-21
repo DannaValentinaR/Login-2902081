5717363b93c81ec0aad936376bd5c807639d4a4c
DannaValentinaR <dvramirez078@misena.edu.co>
2024-07-20 23:35:53 -0500

    creacion del proyecto

--- app/src/main/java/co/edu/sena/com/example/login2902081/screeans/SignUpScreen.kt
+++ app/src/main/java/co/edu/sena/com/example/login2902081/screeans/SignUpScreen.kt
@@ -1,23 +1,37 @@
 package co.edu.sena.com.example.login2902081.screeans
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import co.edu.sena.com.example.login2902081.R
+import co.edu.sena.com.example.login2902081.components.HeadingTextComponent
+import co.edu.sena.com.example.login2902081.components.MyTextFieldComponent
 import co.edu.sena.com.example.login2902081.components.NormalTextComponent
 
 @Composable
 fun SignUpScreen() {
 
     Surface(
-        color = Color.White,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(28.dp)
     ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+
+        }
         NormalTextComponent(value = stringResource(id = R.string.hello))
+        HeadingTextComponent(value = stringResource(id = R.string.create_account))
+
+        MyTextFieldComponent(labelValue = stringResource(id = R.string.first.name))
     }
 }//SignUpScreen
 

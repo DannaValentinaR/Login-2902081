9d9c7370096019a85b9891256fa2a406bc7a53ff
DannaValentinaR <dvramirez078@misena.edu.co>
2024-07-20 21:24:45 -0500

    feature:se crea SignUpScreen en screeans

--- app/src/main/java/co/edu/sena/com/example/login2902081/screeans/SignUpScreen.kt
+++ app/src/main/java/co/edu/sena/com/example/login2902081/screeans/SignUpScreen.kt
@@ -1,4 +1,28 @@
 package co.edu.sena.com.example.login2902081.screeans
 
-class SignUpScreen {
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.sena.com.example.login2902081.R
+import co.edu.sena.com.example.login2902081.components.NormalTextComponent
+
+@Composable
+fun SignUpScreen() {
+
+    Surface(
+        color = Color.White,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        NormalTextComponent(value = stringResource(id = R.string.hello))
+    }
+}//SignUpScreen
+
+@Preview
+@Composable
+fun SingUpScreenPreview() {
+    SignUpScreen()
 }
